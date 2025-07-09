d6a078df8269129d6d1dc050fb9e5e6bf86678a8
Rishitha23092006 <rishithabairy1@gmail.com>
2025-07-09 14:14:22 +0530

    changes in docker image

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node App!');
+  res.send('Hello from Docker!');
 });
 
 app.listen(PORT, () => {
